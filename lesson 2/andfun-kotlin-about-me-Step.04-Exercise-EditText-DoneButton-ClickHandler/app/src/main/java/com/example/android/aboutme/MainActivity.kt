94ba3cf8ee57370974157fed0705c598f2b41275
Phạm Văn Hoàn <codegeass183@gmail.com>
2021-04-22 19:38:09 +0700

    up lesson 2

---
+++ lesson 2/andfun-kotlin-about-me-Step.04-Exercise-EditText-DoneButton-ClickHandler/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Main Activity of the AboutMe app. This app demonstrates:
+ *     * LinearLayout with TextViews, ImageView, Button, EditText, and ScrollView
+ *     * ScrollView to display scrollable text
+ *     * Getting user input with an EditText.
+ *     * Click handler for a Button to retrieve text from an EditText and set it in a TextView.
+ *     * Setting the visibility status of a view.
+ *     * Data binding between MainActivity and activity_main.xml. How to remove findViewById,
+ *       and how to display data in views using the data binding object.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            clickDoneButton(it)
+        }
+
+        // TODO (04) Add a click handler for the Done button to process the input and update the visibility of the views
+    }
+    private fun clickDoneButton(view: View){
+        val editText = findViewById<EditText>(R.id.edit_text)
+        val hidetext = findViewById<TextView>(R.id.hide_text)
+        hidetext.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        hidetext.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
